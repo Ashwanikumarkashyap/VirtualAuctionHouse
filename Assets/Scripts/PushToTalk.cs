800b9d316e8af6043ba9a603dc921e50e0a4a7ac
ashwanikumarkashyap <ashwanikumarkashyap22@gmail.com>
2021-04-19 03:18:55 -0500

    added push to talk

---
+++ Assets/Scripts/PushToTalk.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Voice.Unity;
+
+public class PushToTalk : MonoBehaviourPun
+{
+    public Recorder VoiceRecorder;
+    private PhotonView view;
+    // Start is called before the first frame update
+    void Start()
+    {
+        view = photonView;
+        VoiceRecorder.TransmitEnabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("v"))
+        {
+            if (view.IsMine)
+            {
+                VoiceRecorder.TransmitEnabled = true;
+            }
+        }
+        else if (Input.GetKeyUp("v"))
+        {
+            if (view.IsMine)
+            {
+                VoiceRecorder.TransmitEnabled = false;
+            }
+        }
+    }
+}
